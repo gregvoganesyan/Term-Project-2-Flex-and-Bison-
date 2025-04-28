5a3f9c5b9321e79b6873ec8221bd4613b0ee6471
gregvoganesyan <gregvoganesyan@gmail.com>
2025-04-28 13:42:27 -0500

    Create calcy.h

---
+++ calcy.h
@@ -0,0 +1,23 @@
+#ifndef CALCY_H
+#define CALCY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ASTNode 
+{
+    char *nodeType;
+    char var;
+    int num;
+    struct ASTNode *left;
+    struct ASTNode *right;
+} ASTNode;
+
+ASTNode* createNumNode(int num);
+ASTNode* createVarNode(char var);
+ASTNode* createOpNode(char* op, ASTNode* left, ASTNode* right);
+void printAST(ASTNode* node, int level);
+void freeAST(ASTNode* node);
+
+#endif
